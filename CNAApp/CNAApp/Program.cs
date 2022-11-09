88e7892eafc7d7ef5b97601637d27b5ebe4da724
voidrobyn <92083124+voidrobyn@users.noreply.github.com>
2022-11-09 09:58:10 +0000

    ha tei  itg hdkjfdsk

---
+++ CNAApp/CNAApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace CNAApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+           
+        }
+    }
+
+    public class Client
+    {
+        private TcpClient tcpClient;
+        NetworkStream m_stream;
+        StreamWriter m_writer;
+        StreamReader m_reader;
+
+        public Client()
+        {
+            TcpClient tcpClient = new TcpClient();
+        }
+
+        public bool Connect(string ipAddress, int port)
+        {
+            try
+            {
+                tcpClient.Connect(ipAddress, port);
+                m_stream = tcpClient.GetStream();
+                m_reader = new StreamReader(m_stream, Encoding.UTF8);
+                m_writer = new StreamWriter(m_stream, Encoding.UTF8);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return false;
+            }
+        }
+
+        public void Run()
+        {
+            string userInput;
+            ProcessServerResponse();
+
+            while ((userInput = Console.ReadLine()) != null)
+            {
+                m_writer.WriteLine();
+                m_writer.Flush();
+
+                ProcessServerResponse();
+                if (userInput == )
+            }
+        }
+
+        private void ProcessServerResponse()
+        {
+
+        }
+    }
+}
---
+++ CNAApp/ServerProj/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace ServerProj
+{
+    internal class Server
+    {
+        static void Main()
+        {
+            Server server = new Server("127.0.0.1", 4444);
+            server.Start();
+            server.Stop();
+        }
+
+        TcpListener m_TcpListener;
+
+        public Server(String ipAddress, int port)
+        {
+            IPAddress ip = IPAddress.Parse(ipAddress);
+            m_TcpListener = new TcpListener(ip, port);
+        }
+
+        public void Start()
+        {
+            m_TcpListener.Start();
+
+            Console.WriteLine("listening...");
+
+            Socket socket = m_TcpListener.AcceptSocket();
+            Console.WriteLine("connection made");
+            ClientMethod(socket);
+        }
+
+        public void Stop()
+        {
+            m_TcpListener.Stop();
+        }
+
+        private void ClientMethod(Socket socket)
+        {
+            string recievedMessage;
+            NetworkStream stream = new NetworkStream(socket, true);
+            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+
+            writer.WriteLine("you have connected to the server");
+            writer.Flush();
+
+            while ((recievedMessage == reader.ReadLine()) != null)
+            {
+                GetReturnMessage(recievedMessage);
+
+                writer.WriteLine(GetReturnMessage(recievedMessage));    
+                writer.Flush ();
+            }
+            socket.Close();
+        }
+
+        private string GetReturnMessage(string code)
+        {
+            return "hello";
+        }
+    }
+}
