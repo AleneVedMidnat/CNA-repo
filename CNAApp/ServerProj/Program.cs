3bb2744a6773e18f8d15173b6c22fc233861c142
voidrobyn <92083124+AleneVedMidnat@users.noreply.github.com>
2022-11-16 11:39:03 +0000

    16/11/22

--- CNAApp/ServerProj/Program.cs
+++ CNAApp/ServerProj/Program.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
-
+using System.Threading.Channels;
+using System.Collections.Concurrent;
 
 namespace ServerProj
 {
@@ -19,6 +20,7 @@ namespace ServerProj
         }
 
         TcpListener m_TcpListener;
+        ConcurrentDictionary<int, ConnectedClient> m_clients;
 
         public Server(String ipAddress, int port)
         {
@@ -29,12 +31,22 @@ namespace ServerProj
         public void Start()
         {
             m_TcpListener.Start();
+            m_clients = new ConcurrentDictionary<int, ConnectedClient>();
+            int clientIndex = 0;
 
-            Console.WriteLine("listening...");
-
-            Socket socket = m_TcpListener.AcceptSocket();
-            Console.WriteLine("connection made");
-            ClientMethod(socket);
+            while (true)
+            {
+                Console.WriteLine("listening...");
+                Socket socket = m_TcpListener.AcceptSocket();
+                ConnectedClient client = new ConnectedClient(socket); ;
+                Console.WriteLine("connection made");
+                int index = clientIndex;
+                clientIndex++;
+                m_clients.TryAdd(index, client);
+                Thread thread = new Thread(() => { ClientMethod(index); });
+                thread.Start();
+            }
+            
         }
 
         public void Stop()
@@ -42,24 +54,20 @@ namespace ServerProj
             m_TcpListener.Stop();
         }
 
-        private void ClientMethod(Socket socket)
+        private void ClientMethod(int index)
         {
-            string recievedMessage;
-            NetworkStream stream = new NetworkStream(socket, true);
-            StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-            StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
+            string recievedMessage;  
+            ConnectedClient client = m_clients[index];
 
-            writer.WriteLine("you have connected to the server");
-            writer.Flush();
-
-            while ((recievedMessage = reader.ReadLine()) != null)
+            while ((recievedMessage = client.Read()) != null)
             {
                 GetReturnMessage(recievedMessage);
 
-                writer.WriteLine(GetReturnMessage(recievedMessage));    
-                writer.Flush ();
+                client.Send(recievedMessage);
             }
-            socket.Close();
+            m_clients[index].Close();
+            ConnectedClient c;
+            m_clients.TryRemove(index, out c);
         }
 
         private string GetReturnMessage(string code)
@@ -67,4 +75,51 @@ namespace ServerProj
             return "hello";
         }
     }
+
+    internal class ConnectedClient 
+    {
+        private Socket m_socket;
+        private NetworkStream m_stream;
+        private StreamReader m_reader;
+        private StreamWriter m_writer;
+        private object m_readLock;
+        private object m_writeLock;
+
+        public ConnectedClient(Socket socket)
+        {
+            m_writeLock = new object();
+            m_readLock = new object();
+            m_socket = socket;
+            m_stream = new NetworkStream(socket, true);
+            m_reader = new StreamReader(m_stream, Encoding.UTF8);
+            m_writer = new StreamWriter(m_stream, Encoding.UTF8);
+        }
+
+        public void Close()
+        {
+            m_stream.Close();
+            m_reader.Close();
+            m_writer.Close();
+            m_socket.Close();
+        }
+
+        public string Read()
+        {
+            lock (m_readLock)
+            {
+                return m_reader.ReadLine();
+            }
+        }
+
+        public void Send(string message)
+        {
+            lock (m_writeLock)
+            {
+                m_writer.WriteLine(message);
+                m_writer.Flush();
+            }
+
+        }
+    }
+
 }
